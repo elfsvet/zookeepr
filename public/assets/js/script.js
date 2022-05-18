a312f63818810026a0c3dee4c76b92ab0879ab53
elfsvet <stepanmatysik@gmail.com>
2022-05-18 16:15:30 -0400

    added a animals.js

---
+++ public/assets/js/script.js
@@ -0,0 +1,54 @@
+const $animalForm = document.querySelector('#animal-form');
+
+const handleAnimalFormSubmit = event => {
+  event.preventDefault();
+
+  // get animal data and organize it
+  const name = $animalForm.querySelector('[name="animal-name"]').value;
+  const species = $animalForm.querySelector('[name="species"]').value;
+  const dietRadioHTML = $animalForm.querySelectorAll('[name="diet"]');
+  let diet;
+
+  for (let i = 0; i < dietRadioHTML.length; i += 1) {
+    if (dietRadioHTML[i].checked) {
+      diet = dietRadioHTML[i].value;
+    }
+  }
+
+  if (diet === undefined) {
+    diet = '';
+  }
+
+  const selectedTraits = $animalForm.querySelector('[name="personality"').selectedOptions;
+  const personalityTraits = [];
+  for (let i = 0; i < selectedTraits.length; i += 1) {
+    personalityTraits.push(selectedTraits[i].value);
+  }
+  const animalObject = { name, species, diet, personalityTraits };
+// instead of url we don't use full url because the request coming from server
+//simular to how we use script and link to incorporate local files, except that now it's being done through a fetch() call!
+  fetch('/api/animals', {
+    // this will allow the request to make it to the proper endpoint in our server to add new animals to the JSON file.
+    method: 'POST',
+    // what type of data we're looking to send and then actually provide the data.  
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    },
+    // We can add stringified JSON data for out animalObject to the body property of the request.
+    // ! Without these, we would never receive req.body on the server!
+    body: JSON.stringify(animalObject)
+  })
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      alert('Error: ' + response.statusText);
+    })
+    .then(postResponse => {
+      console.log(postResponse);
+      alert('Thank you for adding an animal!');
+    });
+};
+
+$animalForm.addEventListener('submit', handleAnimalFormSubmit);
--- server.js
+++ server.js
@@ -25,6 +25,11 @@ app.use(express.json());
 // ! Both of the above middleware functions need to be set up every time you create a server that's looking to accept POST data.
 
 
+// !Every time we create a server that will serve a front end as well as JSON data, we'll always want to use this middleware. 
+app.use(express.static('public'));
+// some more middleware static method. We provide a file path to a location in our application(public folder) and instruct the server to make these files static resources.
+// This means that all of our front-end code can now be accessed without having a specific server endpoint created for it!
+
 function filterByQuery(query, animalsArray) {
 
     let personalityTraitsArray = [];
@@ -155,6 +160,25 @@ app.post('/api/animals', (req, res) => {
 
 });
 // we created a route here. POST request represents the action of a client requesting the server to accept data rather than vice versa (another way around)
+// '/' brings as to the root route of our server. This is the route used to create a homepage for a server
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+// this route will take us to animals/when we create routes we need to stay organized and set expectations of what type of data is being transferred at that endpoint.
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+// linking between pages no longer needs html extensions. it just needs the associated route name. it also doesn't need the Open in Browser extension!
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+// we * will act as a wildcard, meaning any route that wasn't previously defined will fall under this request and will receive the homepage as the response.
+// Thus, requests for /about or /contact or /membership will essentially be the same now
+// ! The order of the routes matters! The * route should always come last. Otherwise, it will take precedence over names routes, and you won't see what you expect to see on routes like /zookeeper
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 
 
@@ -162,5 +186,3 @@ app.post('/api/animals', (req, res) => {
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
-
-// !left at 11.2.5 in the end 11.2.4 didn't much the picture.
